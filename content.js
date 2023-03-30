3e6191c3c8b808bb17cad7eacb5c522a046c49f3
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-03-30 16:32:58 +0530

    Initial commit

---
+++ content.js
@@ -0,0 +1,36 @@
+let totalTime = {
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+};
+
+document.querySelectorAll("ytd-playlist-video-renderer").forEach((element) => {
+  try {
+    let timelist = element
+      .querySelector("#overlays")
+      .querySelector("ytd-thumbnail-overlay-time-status-renderer")
+      .querySelector("span")
+      .firstChild.data.trim()
+      .split(":");
+
+    if (timelist.length === 2) {
+      totalTime.minutes += parseInt(timelist[0]);
+      totalTime.seconds += parseInt(timelist[1]);
+    } else if (timelist.length === 3) {
+      totalTime.hours += parseInt(timelist[0]);
+      totalTime.minutes += parseInt(timelist[1]);
+      totalTime.seconds += parseInt(timelist[2]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+totalTime.minutes += parseInt(totalTime.seconds / 60);
+totalTime.seconds = totalTime.seconds % 60;
+
+totalTime.hours += parseInt(totalTime.minutes / 60);
+totalTime.minutes = totalTime.minutes % 60;
+
+var msg = `Total time of playlist: ${totalTime.hours} hours, ${totalTime.minutes} minutes, ${totalTime.seconds} seconds`; 
+alert(msg);
