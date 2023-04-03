83ec71827ec657e06d065f5afb5f96d628a1e154
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-04-03 22:59:00 +0530

    Minor bug fix

--- popup.js
+++ popup.js
@@ -16,7 +16,7 @@ callCalculate = (tab) => {
   }
   
   getCurrentTab = async () => {
-    let queryOptions = { active: true};
+    let queryOptions = { active: true, currentWindow: true};
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
   }
